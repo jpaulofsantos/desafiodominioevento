54ddf8a45fbe028de8cf002756b2936cf94db4ba
JoaoPaulo <jpaulofsantos@gmail.com>
2023-12-08 20:54:17 -0300

    commit desafio

---
+++ src/main/java/com/desafio/desafiodominioevento/DesafiodominioeventoApplication.java
@@ -0,0 +1,13 @@
+package com.desafio.desafiodominioevento;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafiodominioeventoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesafiodominioeventoApplication.class, args);
+	}
+
+}
